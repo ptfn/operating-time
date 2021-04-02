79d1a079dff7e5669b19c71018f59a34cd5d85e6
ptfn <CipherDogs@protonmail.com>
2021-04-02 16:51:27 +0700

    fix

--- Scala/HelloWorld.scala
+++ Scala/HelloWorld.scala
@@ -1,3 +1,7 @@
- object HelloWorld extends App {
-   println("Hello, World!")
- }
+object HelloWorld extends App {
+    def factorial(n: Int): Long = {
+        if(n>1) n * factorial(n-1)
+        else 1
+    }
+    println(factorial(40))
+}
