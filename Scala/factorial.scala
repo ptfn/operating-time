58357b1d487ca210f42e81e0979c5109bcce9777
ptfn <CipherDogs@protonmail.com>
2021-04-22 17:40:16 +0700

    added file and fixed

--- Scala/factorial.scala
+++ Scala/factorial.scala
@@ -1,6 +1,6 @@
 object factorial extends App {
-    def factorial(n: Int): Long = {
+    def factorial(n: BigInt): BigInt = {
         if(n>1) n * factorial(n-1) else 1
     }
-    println(factorial(40))
+    println(factorial(30))
 }
