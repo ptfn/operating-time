0c3316ddc3d3a6eac3442282f884696aa3172e83
ptfn <CipherDogs@protonmail.com>
2021-04-23 21:17:41 +0700

    fixed

--- Scala/pi.scala
+++ Scala/pi.scala
@@ -1,17 +1,17 @@
 object pi extends App{
     def pi(n: Int): Double = {
-        var num: Double = 4.0
-        var den: Double = 1.0
-        var oper: Double = 1.0
-        var pi: Double = 0.0  
+        var num = 4.0
+        var den = 1.0
+        var oper = 1.0
+        var pi = 0.0  
         var i = n
         while (i > 0){
-            pi = pi + oper * (num / den)
-            den = den + 2.0
-            oper = oper * -1.0
-            i = i - 1
+            pi += oper * (num / den)
+            den += 2.0
+            oper *= -1.0
+            i -= 1
             }
         return pi
     }
-    println("Pi:" + pi(999999))
+    println("Pi:" + pi(args(0).toInt))
 }
