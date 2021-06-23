a420d02503a221c26f74541e2d48c67127a7d26c
ptfn <CipherDogs@protonmail.com>
2021-06-23 14:26:31 +0700

    deleted code

--- Python/Password.py
+++
@@ -1,9 +0,0 @@
-import random
-Symbols = "!@#$%^&*=<>0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-Length = int(input("Введите длину пароля:"))
-Num_pass = int(input("Введите число паролей"))
-for n in range(Num_pass):
-    password = ""
-    for i in range(Length):
-        password += random.choice(Symbols)
-    print(password)
