a9817f94b3bf4e5624cf1734ddc5bc499baff68f
ptfn <CipherDogs@protonmail.com>
2021-07-05 11:07:06 +0700

    added code

---
+++ Python/pow.py
@@ -0,0 +1,18 @@
+def pow(n, m):
+    arr = []
+    res = 1
+
+    while True:
+        if m % 2 != 0:
+            arr.append(n)
+        if m == 1:
+            break
+        m = m // 2
+        n = n ** 2
+
+    for i in range(len(arr)):
+        res *= arr[i]
+    return res
+
+if __name__ == '__main__':
+    print(pow(102, 100))
