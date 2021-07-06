7ca6d3625a5d03600713271cc3a46eb6fb64860a
ptfn <CipherDogs@protonmail.com>
2021-07-06 11:46:29 +0700

    added code

---
+++ Python/even.py
@@ -0,0 +1,20 @@
+def even(n):
+    arr = []
+    arr2 = []
+    res = 0
+    # str = ""
+    for i in range(1, n+1):
+        arr.append(i)
+    for i in range(1, len(arr)):
+        for j in range(1, len(arr)):
+            res = i+j+2*i*j
+            if res <= len(arr):
+                arr[res-1] = 0
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            # str += "{} ".format(2*arr[i]+1)
+            arr2.append(2*arr[i]+1)
+    return arr2
+
+if __name__ == '__main__':
+    print(even(100))
