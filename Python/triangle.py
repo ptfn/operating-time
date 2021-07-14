76d28e785ffa5f561f0401581e90b4f0d62d3437
ptfn <CipherDogs@protonmail.com>
2021-07-14 10:30:40 +0700

    fix

--- Python/triangle.py
+++ Python/triangle.py
@@ -9,10 +9,11 @@ class Point():
 X = [0]
 Y = [0]
 
-a = Point(random.randrange(10, 50), random.randrange(10, 50))
-b = Point(random.randrange(10, 50), random.randrange(10, 50))
-c = Point(random.randrange(10, 50), random.randrange(10, 50))
-first = Point(random.randrange(20, 40), random.randrange(20, 40))
+a = Point(random.randrange(-15, 15), random.randrange(-15, 15))
+b = Point(random.randrange(-15, 15), random.randrange(-15, 15))
+c = Point(random.randrange(-15, 15), random.randrange(-15, 15))
+first = Point(random.randrange(-5, 5), random.randrange(-5, 5))
+
 
 def dotPoint(p1:Point, p2:Point):
     if p1.x > p2.x: 
@@ -26,19 +27,21 @@ def dotPoint(p1:Point, p2:Point):
         y = ((p2.y - p1.y) / 2) + p1.y
     return Point(x,y)
 
-for i in range(10000):
-    random_dot = random.randrange(0,100)
+for i in range(99999):
+    random_dot = random.randrange(0,300)
 
-    if random_dot > 0 & random_dot < 33:
+    if random_dot >= 0 & random_dot <= 99:
         newp = dotPoint(p1 = a, p2 = first)
         first = newp
-    if random_dot > 34 & random_dot < 67:
+        X.append(newp.x);Y.append(newp.y)
+    elif random_dot >= 100 & random_dot <= 200:
         newp = dotPoint(p1 = b, p2 = first)
         first = newp
-    if random_dot > 68 & random_dot < 100:
+        X.append(newp.x);Y.append(newp.y)
+    elif random_dot >= 201 & random_dot <= 300:
         newp = dotPoint(p1 = c, p2 = first)
         first = newp
-    X.append(newp.x);Y.append(newp.y)
+        X.append(newp.x);Y.append(newp.y)
     
 
 plt.figure(figsize = [15,15])
