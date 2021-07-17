d1fecfa74c48532ce69966f32edb6b5cec98fe8a
ptfn <CipherDogs@protonmail.com>
2021-07-17 21:28:00 +0700

    fix

--- Python/rainbow-table.py
+++ Python/rainbow-table.py
@@ -1,26 +1,30 @@
 import hashlib
 
-chars = "abcdefghijklmnopqrstuvwxyz"
+chars = "0123456789"
 chars_len = len(chars)
+pass_len = 6
+chain_len = 1000
+chain_num = 100000
+
 
 def reduce(i):
     pwd = ""
 
-    while len(pwd) < 8:
+    while len(pwd) < pass_len:
         pwd = pwd + chars[ i % chars_len ]
         i = i // chars_len
     return pwd
 
 file = open("table.txt", 'a+', encoding='utf-8')
 
-for s in range(0, 100):
+for s in range(0, chain_num):
     start = reduce(s)
     password = start
 
-    for i in range(0, 10000):
+    for i in range(0, chain_len):
         hash_pass = hashlib.md5(password.encode('ascii')).hexdigest()
         password = reduce(int(hash_pass, 16))
 
-    file.write('{0} -> {1}\n'.format(start, password))
+    file.write('{}\n'.format(password))
 
 file.close()
