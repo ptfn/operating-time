1cc244e9d1adb93d586a1c42a3b9c92accc06622
ptfn <CipherDogs@protonmail.com>
2021-09-12 15:53:22 +0700

    fix

--- Python/prime_numbers.py
+++ Python/prime_numbers.py
@@ -1,22 +1,24 @@
-def even(n):
-    arr = []
-    arr2 = []
-    res = 0
+import time
 
-    for i in range(1, n+1):
-        arr.append(i)
+n = 1650
+arr = []
 
-    for i in range(1, len(arr)):
-        for j in range(1, len(arr)):
-            res = i+j+2*i*j
-            if res <= len(arr):
-                arr[res-1] = 0
+for i in range(1, n+1):
+    arr.append(i)
 
-    for i in range(len(arr)):
-        if arr[i] != 0:
-            arr2.append(2*arr[i]+1)
+for i in range(1, len(arr)):
+    for j in range(1, len(arr)):
+        res = i+j+2*i*j
+        if res <= len(arr):
+            arr[res-1] = 0
 
-    return arr2
-
-if __name__ == '__main__':
-    print(even(100))
+for i in range(len(arr)):
+    if (2*arr[i]+1) == 1033:
+        print(2*arr[i]+1)
+        time.sleep(2)
+    elif (2*arr[i]+1) == 3301:
+        print(2*arr[i]+1)
+        time.sleep(2)
+    elif arr[i] != 0:
+        print(2*arr[i]+1)
+        time.sleep(0.05)
