09dfdd00b28a24cbb09a65115fa9f4be7a45ba11
ptfn <CipherDogs@protonmail.com>
2021-10-03 23:15:45 +0700

    fix

--- Haskell/fac.hs
+++ Haskell/fac.hs
@@ -3,4 +3,4 @@ fac 0 = 1
 fac n | n > 0 = n * fac (n - 1)
 
 main :: IO()
-main = print(fac 1000^1000)
+main = print(fac 1000)
--- Python/Pi.py
+++ Python/Pi.py
@@ -3,9 +3,12 @@ def pi(n:int):
   den: float = 1.0
   oper: float = 1.0
   pi:float = 0.0
+
   for i in range(n):
     pi += oper * (num / den)
     den += 2.0
     oper *= -1.0
+
   return pi
+
 print(pi(500000))
