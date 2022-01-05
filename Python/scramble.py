abbb365e167b46000405c0aeccaa4b9c24de35e3
ptfn <CipherDogs@protonmail.com>
2022-01-05 18:53:49 +0700

    new code

---
+++ Python/scramble.py
@@ -0,0 +1,51 @@
+import tkinter
+import random
+
+def start():
+    root.after_idle(loop, 0)   
+
+def swap():
+    scramble['text'] = get_scramble()
+
+def get_scramble():
+    scramble_length = random.randint(20, 25)
+    moves = ["R", "R'", "R2", "L", "L'", "L2", "U", "U'", "U2", "D", "D'", "D2", "F", "F'", "F2", "B", "B'", "B2"]
+    scramble = ""
+    for i in range(0, scramble_length):
+        random_move = random.randint(0, len(moves) - 1)
+        if i > 0:
+            while moves[random_move][0] == prev_move[0]:
+                random_move = random.randint(0, len(moves) - 1)
+        scramble += " " + moves[random_move]
+        prev_move = moves[random_move]
+    return scramble.strip()
+
+def format(t):
+    global time
+    milliseconds = int(t % 10)
+    sec = (t - milliseconds) / 10
+    minutes = int(sec // 60)
+    seconds = int(sec % 60)
+    return str(minutes) + ':' + str(seconds) + ':' +str(milliseconds)
+
+def loop(n):
+        label['text'] = str(format(n))
+        root.after(100, loop, n+1)
+
+root = tkinter.Tk()
+    
+label = tkinter.Label(font=(None, 100))
+label['text'] = '0:0:0'
+label.pack()
+
+scramble = tkinter.Label(font=(None, 30))
+scramble['text'] = get_scramble()
+scramble.pack()
+
+button = tkinter.Button(root, text="SWAP", command=swap)
+button.pack()
+
+button2 = tkinter.Button(root, text="START", command=start) 
+button2.pack()
+
+root.mainloop()
