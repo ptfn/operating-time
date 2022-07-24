defffed5cb1633020d1459b1e409eceee2aa6853
ptfn <CipherDogs@protonmail.com>
2022-07-24 14:24:25 +0700

    fixed

--- python/mat.py
+++ python/mat.py
@@ -7,7 +7,7 @@ def gen_num(number, n):
 
 def main():
     dictionary = {str(x+1):0 for x in range(6)}
-    gen_num(dictionary, 100000)
+    gen_num(dictionary, 600000)
     print("Число\tЧастота")
     for i in range(1,len(dictionary)+1):
         print(f"{i}\t{dictionary[str(i)]}")
